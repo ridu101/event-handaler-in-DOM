edd72dbd61bfa86adadd2d83d43448978411bc54
ridu101 <ridu116540@gmail.com>
2025-08-31 10:20:01 +0600

    on click bg color added

--- js/even.js
+++ js/even.js
@@ -1,1 +1,15 @@
 console.log('External File')
+
+// button event handler 
+
+ function makeYellow(){
+    document.body.style.backgroundColor='yellow';
+
+ }
+ function makeBlue(){
+    document.body.style.backgroundColor= 'blue';
+ }
+
+ function makeRed(){
+    document.body.style.backgroundColor= 'red';
+ }
